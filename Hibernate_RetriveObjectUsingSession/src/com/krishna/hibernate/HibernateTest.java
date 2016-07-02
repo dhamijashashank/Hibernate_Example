3d539457ecd94b34fc9b85fabdf8538777bb7087
Shashank <dhamija.shashank@gmail.com>
2016-07-02 10:12:15 +0530

    Example of save and retrive of object in/from Database (Postgres)

---
+++ Hibernate_RetriveObjectUsingSession/src/com/krishna/hibernate/HibernateTest.java
@@ -0,0 +1,42 @@
+package com.krishna.hibernate;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.krishna.hibernate.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		UserDetails usd = new UserDetails();
+		usd.setUserId(1);
+		usd.setUserName("Hare Krishna Hare Rama");
+		usd.setAddress("Address1 ");
+		usd.setDescription("description1 ");
+		usd.setJoiningDate(new Date());
+		
+		SessionFactory buildSessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = buildSessionFactory.openSession();
+		session.beginTransaction();
+		session.save(usd);
+		session.getTransaction().commit();
+		session.close();
+		
+		
+		usd = null;
+		session = buildSessionFactory.openSession();
+		session.beginTransaction();
+		usd = session.get(UserDetails.class, 1);
+		System.out.println("usd name is "+ usd.getUserName()); 
+		session.close();
+		
+		
+
+	}
+
+}
