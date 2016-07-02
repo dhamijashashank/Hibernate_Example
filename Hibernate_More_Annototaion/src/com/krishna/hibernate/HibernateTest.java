d5c2b251633ed2c86b3492e01977ab2f40551b67
Shashank <dhamija.shashank@gmail.com>
2016-07-02 10:07:03 +0530

    Upload Example of Hibernate annotations

---
+++ Hibernate_More_Annototaion/src/com/krishna/hibernate/HibernateTest.java
@@ -0,0 +1,38 @@
+package com.krishna.hibernate;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.krishna.hibernate.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		UserDetails usd = new UserDetails();
+		usd.setUserName("Hare Krishna Hare Rama");
+		usd.setAddress("Address6 ");
+		usd.setDescription("description6 ");
+		usd.setJoiningDate(new Date());
+		
+		UserDetails usd1 = new UserDetails();
+		usd1.setUserName("Hare Krishna Hare Rama");
+		usd1.setAddress("Address ");
+		usd1.setDescription("description ");
+		usd1.setJoiningDate(new Date());
+		
+		SessionFactory buildSessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = buildSessionFactory.openSession();
+		session.beginTransaction();
+		session.save(usd);
+		session.save(usd1);
+		session.getTransaction().commit();
+		session.close();
+
+	}
+
+}
---
+++ Hibernate_More_Annototaion/src/com/krishna/hibernate/dto/UserDetails.java
@@ -0,0 +1,69 @@
+package com.krishna.hibernate.dto;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity (name="USER_DETAIL")
+public class UserDetails {
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	private int userId;
+//	@Basic(fetch=FetchType.LAZY)//@Basic(fetch=FetchType.EAGER)//
+	private String userName;
+	//If user want that feild is not store in DB make it static or Transiant or use below annotation
+//	@Transient
+	private String address;
+	 
+//	@Temporal(TemporalType.TIMESTAMP)// by Default
+//	@Temporal(TemporalType.TIME)//if user want to store Time On
+	@Temporal(TemporalType.TIMESTAMP)// if user want to store Date only
+	private Date joiningDate;
+	
+	private String description;
+	
+	
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getUserId() {
+		return userId;
+	}	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
